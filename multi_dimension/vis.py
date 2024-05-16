7bc9d25062bdcd2802ec9d45d97ea375b1cc87e8
Andreas Hartl-Bachinger <andreas.hartl-bachinger@gmx.at>
2024-05-16 19:24:10 +0200

    update plot-label

--- multi_dimension/vis.py
+++ multi_dimension/vis.py
@@ -6,5 +6,5 @@ df = pd.read_csv("data.csv")
 fig, ax = plt.subplots(2, 1)
 
 df.plot(x="Time", y=["Value_0", "Value_1"], ax=ax[0])
-df.plot(x="Value_0", y="Value_1", ax=ax[1])
+df.plot(x="Value_0", y="Value_1", ax=ax[1], label='xy')
 fig.savefig("plot.png")
